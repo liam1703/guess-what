dd33137b4cd5a49172b9b87379737dbb3dd08bfa
Liam Bradbury <liam.bradbury@nurole.com>
2022-04-29 17:23:25 +0100

    Import css into app

--- predict2win/src/main.js
+++ predict2win/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
+import "./assets/index.css";
 
 let app = createApp(App);
 app.use(router).mount("#app");
