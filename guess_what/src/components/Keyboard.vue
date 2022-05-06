cf7f1e1c0fbe641be082d577e2bf0bf8032e0779
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-06 17:30:28 +0100

    Keyboard keys in data

--- guess_what/src/components/Keyboard.vue
+++ guess_what/src/components/Keyboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex">
     <div class="p-4 bg-green-300 mr-2">a</div>
-    <div class="p-4 bg-green-300 mr-2">a</div>
+    <div class="p-4 bg-green-300 mr-2"></div>
     <div class="p-4 bg-green-300 mr-2">a</div>
     <div class="p-4 bg-green-300 mr-2">a</div>
     <div class="p-4 bg-green-300 mr-2">a</div>
@@ -15,5 +15,12 @@
 <script>
 export default {
   name: "Keyboard",
+  data() {
+    return {
+      firstRowKeys: ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+      secondRowKeys: ["A", "S", "D", "F", "G", "H", "J", "K", "L"],
+      thirdRowKeys: ["ENTER", "Z", "X", "C", "V", "B", "N", "M", "DELETE"],
+    };
+  },
 };
 </script>
