4c5084fde3bc658a9c6e9737a8d8c2ad35b1f91f
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-06 16:55:53 +0100

    Nav

--- guess_what/src/components/TopNav.vue
+++ guess_what/src/components/TopNav.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="h-8 bg-gray-800 w-full"></div>
+  <div
+    class="h-20 bg-gray-900 w-full flex justify-center items-center border-b-2 border-green-300"
+  >
+    <h1 class="text-3xl text-white text-center">Guess What</h1>
+  </div>
 </template>
 
 <script>
