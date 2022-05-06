1c1336103fdfbcf98c8dd8d85a0a08a260dd1425
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-06 17:16:30 +0100

    Import keyboard

--- guess_what/src/views/Game.vue
+++ guess_what/src/views/Game.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="bg-gray-800 w-screen h-screen">
     <TopNav />
-    <div class="h-1/2 items-center">
+    <div class="h-1/2">
       <Board />
     </div>
+    <Keyboard />
   </div>
 </template>
 
 <script>
 import TopNav from "../components/TopNav.vue";
 import Board from "../components/Board.vue";
+import Keyboard from "../components/Keyboard.vue";
 export default {
   name: "Game",
   components: {
     TopNav,
     Board,
+    Keyboard,
   },
 };
 </script>
