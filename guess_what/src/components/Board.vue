708eda578eca29c67c2e2a031ddb8543488fd423
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-06 17:03:44 +0100

    Import board component

--- guess_what/src/components/Board.vue
+++ guess_what/src/components/Board.vue
@@ -1,4 +1,6 @@
-<template></template>
+<template>
+  <div>hello</div>
+</template>
 
 <script>
 export default {
--- guess_what/src/views/Game.vue
+++ guess_what/src/views/Game.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="bg-gray-800 w-screen h-screen">
     <TopNav />
+    <Board />
   </div>
 </template>
 
 <script>
 import TopNav from "../components/TopNav.vue";
+import Board from "../components/Board.vue";
 export default {
   name: "Game",
   components: {
     TopNav,
+    Board,
   },
 };
 </script>
