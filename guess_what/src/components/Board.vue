e43636582c3bcbcd93420956071742e87c73c6c8
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-06 17:05:58 +0100

    Create letter boxes

--- guess_what/src/components/Board.vue
+++ guess_what/src/components/Board.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>hello</div>
+  <div class="flex">
+    <div class="border-2 border-green-300 p-2 w-12 h-12"></div>
+    <div class="border-2 border-green-300 p-2 w-12 h-12"></div>
+    <div class="border-2 border-green-300 p-2 w-12 h-12"></div>
+    <div class="border-2 border-green-300 p-2 w-12 h-12"></div>
+    <div class="border-2 border-green-300 p-2 w-12 h-12"></div>
+  </div>
 </template>
 
 <script>
