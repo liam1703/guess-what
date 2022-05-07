eddbd15e2c279d985fb76b33abda91476a43a497
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-07 15:42:18 +0100

    Just fill letters in on correct row

--- guess_what/src/components/Board.vue
+++ guess_what/src/components/Board.vue
@@ -6,11 +6,11 @@
       :key="index"
     >
       <div
-        class="border-2 border-green-300 p-2 w-16 h-16 mr-2"
+        class="border-2 border-green-300 w-16 h-16 mr-2 text-center flex justify-center items-center text-white text-3xl"
         v-for="(letters, id) in currentGuess"
         :key="id"
       >
-        {{ guesses.first[id] }}
+        {{ getCorrectLetter(index, id) }}
       </div>
     </div>
   </div>
@@ -25,6 +25,34 @@ export default {
       finalGuesses: ["", "", "", "", "", ""],
       currentGuess: [[], [], [], [], []],
     };
+  },
+  methods: {
+    getCorrectLetter(row, ind) {
+      let pre = "";
+      switch (row) {
+        case 0:
+          pre = "first";
+          break;
+        case 1:
+          pre = "second";
+          break;
+        case 2:
+          pre = "third";
+          break;
+        case 3:
+          pre = "fourth";
+          break;
+        case 4:
+          pre = "fifth";
+          break;
+        case 5:
+          pre = "sixth";
+          break;
+        default:
+          pre = null;
+      }
+      return this.guesses[pre][ind];
+    },
   },
 };
 </script>
--- guess_what/src/views/Game.vue
+++ guess_what/src/views/Game.vue
@@ -27,6 +27,7 @@ export default {
         third: [],
         fourth: [],
         fifth: [],
+        sixth: [],
       },
       LetterGuess: "",
       numberOfWordsGuessed: 0,
