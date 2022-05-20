f0e5444aeac8751e4543217d112c8359e22a4686
Liam Bradbury <liam.bradbury@nurole.com>
2022-05-20 21:24:15 +0100

    Modal styling

--- guess_what/src/components/CompletedModal.vue
+++ guess_what/src/components/CompletedModal.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="w-screen h-screen bg-gray-100 opacity-10">
+  <div
+    class="fixed w-screen h-screen z-80 inset-0 bg-gray-900 bg-opacity-90 backdrop-blur"
+  >
     <div class="w-full h-full flex justify-center text-center">
-      <div class="w-1/3 h-3/5 bg-purple-500 my-auto brightness-200">
+      <div class="w-1/3 h-3/5 bg-purple-500 my-auto inset-0">
         <h1>HELLO</h1>
       </div>
     </div>
