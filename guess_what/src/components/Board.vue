443c7a127cdda3112b976194bfbdee01a886c728
Liam Bradbury <liam.bradbury98@gmail.com>
2022-08-06 15:15:14 +0100

    Sort out some positioning

--- guess_what/src/components/Board.vue
+++ guess_what/src/components/Board.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="mt-20">
+  <div class="mt-10">
     <div
       class="flex justify-center w-full mb-4"
       v-for="(item, index) in finalGuesses"
       :key="index"
     >
       <div
-        class="border-2 border-green-300 w-16 h-16 mr-2 text-center flex justify-center items-center text-white text-3xl"
+        class="border-2 border-green-300 w-12 h-12 mr-2 text-center flex justify-center items-center text-white text-3xl"
         v-for="(letters, id) in currentGuess"
         :key="id"
         :class="getAnswerClass(index, id)"
@@ -18,79 +18,79 @@
 </template>
 
 <script>
-export default {
-  name: "Board",
-  props: ["guesses", "results"],
-  data() {
-    return {
-      finalGuesses: ["", "", "", "", "", ""],
-      currentGuess: [[], [], [], [], []],
-    };
-  },
-  methods: {
-    getCorrectLetter(row, ind) {
-      let pre = "";
-      switch (row) {
-        case 0:
-          pre = "first";
-          break;
-        case 1:
-          pre = "second";
-          break;
-        case 2:
-          pre = "third";
-          break;
-        case 3:
-          pre = "fourth";
-          break;
-        case 4:
-          pre = "fifth";
-          break;
-        case 5:
-          pre = "sixth";
-          break;
-        default:
-          pre = null;
-      }
-      return this.guesses[pre][ind];
+  export default {
+    name: "Board",
+    props: ["guesses", "results"],
+    data() {
+      return {
+        finalGuesses: ["", "", "", "", "", ""],
+        currentGuess: [[], [], [], [], []],
+      };
     },
-    getCorrectResult(row, ind) {
-      let pre = "";
-      switch (row) {
-        case 0:
-          pre = "first";
-          break;
-        case 1:
-          pre = "second";
-          break;
-        case 2:
-          pre = "third";
-          break;
-        case 3:
-          pre = "fourth";
-          break;
-        case 4:
-          pre = "fifth";
-          break;
-        case 5:
-          pre = "sixth";
-          break;
-        default:
-          pre = null;
-      }
-      return this.results[pre][ind];
-    },
+    methods: {
+      getCorrectLetter(row, ind) {
+        let pre = "";
+        switch (row) {
+          case 0:
+            pre = "first";
+            break;
+          case 1:
+            pre = "second";
+            break;
+          case 2:
+            pre = "third";
+            break;
+          case 3:
+            pre = "fourth";
+            break;
+          case 4:
+            pre = "fifth";
+            break;
+          case 5:
+            pre = "sixth";
+            break;
+          default:
+            pre = null;
+        }
+        return this.guesses[pre][ind];
+      },
+      getCorrectResult(row, ind) {
+        let pre = "";
+        switch (row) {
+          case 0:
+            pre = "first";
+            break;
+          case 1:
+            pre = "second";
+            break;
+          case 2:
+            pre = "third";
+            break;
+          case 3:
+            pre = "fourth";
+            break;
+          case 4:
+            pre = "fifth";
+            break;
+          case 5:
+            pre = "sixth";
+            break;
+          default:
+            pre = null;
+        }
+        return this.results[pre][ind];
+      },
 
-    getAnswerClass(row, ind) {
-      const num = this.getCorrectResult(row, ind);
-      if (num == 2) {
-        return `bg-green-600`;
-      } else if (num == 1) {
-        return `bg-yellow-600`;
-      } else if (num == 0) {
-        return `bg-gray-600`;
-      }
+      getAnswerClass(row, ind) {
+        const num = this.getCorrectResult(row, ind);
+        if (num == 2) {
+          return `bg-green-600`;
+        } else if (num == 1) {
+          return `bg-yellow-600`;
+        } else if (num == 0) {
+          return `bg-gray-600`;
+        }
+      },
     },
-  },
-};
+  };
 </script>
--- guess_what/src/components/CompletedModal.vue
+++ guess_what/src/components/CompletedModal.vue
@@ -3,15 +3,16 @@
     class="fixed w-screen h-screen z-80 inset-0 bg-gray-900 bg-opacity-90 backdrop-blur"
   >
     <div class="w-full h-full flex justify-center text-center">
-      <div class="w-1/3 h-3/5 bg-purple-500 my-auto inset-0">
-        <h1>HELLO</h1>
+      <div class="w-1/3 h-3/5 bg-gray-600 my-auto inset-0">
+        <h1 class="text-white text-2xl">HELLO</h1>
+        <div class="border-white border-2 w-max h-max">PLAY AGAIN</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "CompletedModal",
-};
+  export default {
+    name: "CompletedModal",
+  };
 </script>
